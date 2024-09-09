207f87ae7f3cf71144293c012a659aa42d392cc4
Michael Krämer <info.m.kraemer@gmail.com>
2024-09-09 17:46:21 +0200

    first services - enter  collection, docs

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Note } from '../interfaces/note.interface';
+import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
+  firestore: Firestore = inject(Firestore); // hier kriege ich zugriff auf den Firestore mit der Variable firestore.
+
   constructor() { }
+
+  // hier kriege ich zugriff auf den Firestore mit der Variable mit collection(this.firestore, 'auswahl')
+  getNotesRef(){
+    return collection(this.firestore, ' notes)');
+  }
+
+  getTrashRef(){
+    return collection(this.firestore, ' trash)');
+  }
+
+  getSingleDocRef(colId: string, docId: string){
+    return doc(collection(this.firestore, colId), docId)
+  }
+   // weil erst collection ausgefuehrt wird und danach erst doc, muss dazwischen ein ','
+   // getSingleDocRef ermoeglicht den Zugriff auf die werte in diesen "ordnern"
+
 }
