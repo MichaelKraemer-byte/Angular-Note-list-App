451be90179dd64a2e5efd59448d58c3dc3cd6815
Michael Krämer <info.m.kraemer@gmail.com>
2024-09-10 15:11:22 +0200

    Read and create doc in Firestore, enable trash and normal note list switch button

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { Note } from '../interfaces/note.interface';
-import { Firestore, collection, doc } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot, addDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,23 +11,101 @@ export class NoteListService {
   trashNotes: Note[] = [];
   normalNotes: Note[] = [];
 
+  // items$;
+  // items;
   firestore: Firestore = inject(Firestore); // hier kriege ich zugriff auf den Firestore mit der Variable firestore.
 
-  constructor() { }
 
-  // hier kriege ich zugriff auf den Firestore mit der Variable mit collection(this.firestore, 'auswahl')
+  unsubNotes;
+  unsubTrash;
+  // unsubSingle;
+
+  constructor() { 
+    this.unsubTrash = this.subTrashList();
+    this.unsubNotes = this.subNotesList();
+  }
+
+  async addNote(item: Note) {
+    await addDoc(this.getNotesRef(), item).catch(
+      (error) => { console.error(error)}
+    ).then(
+      (docRef) => {    
+        console.log("Document written with ID: ", docRef?.id);
+      }
+    )
+  } // addDoc(Reference(in diesem fall 'notes' oder 'trash'), item (das hinzugefuegt werden soll)) - dann .catch & .then block als ueberpruefung (try-cach-block) geht auch)
+
+  subTrashList(){
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data()));
+      });
+    });
+
+    // bei onSnapshot kriege ich die ganzen dateien mit .data() 
+    // andernfalls nur mit 'element' kriege ich alle dateien der collection token (id, existss, ...)
+
+    // unsubList wird definiert, um spaeter den subscription process wieder zu beenden. hier im constructor wird er mit onSnapshot direkt geschrieben,
+    // um die Daten als erstes zu bekommen und den Prozess in den Gang zu schieben. 
+    // beendet werden kann der process mit ngOnDestroy.
+  }
+
+  subNotesList(){
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data()));
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.unsubTrash) {
+      this.unsubTrash();  // Beendet die Echtzeit-Verbindung zu Firestore
+    }
+    if (this.unsubNotes) {
+      this.unsubNotes()
+    }
+  }
+
   getNotesRef(){
-    return collection(this.firestore, ' notes)');
+    return collection(this.firestore, 'notes');
   }
 
   getTrashRef(){
-    return collection(this.firestore, ' trash)');
+    return collection(this.firestore, 'trash');
   }
 
   getSingleDocRef(colId: string, docId: string){
     return doc(collection(this.firestore, colId), docId)
   }
-   // weil erst collection ausgefuehrt wird und danach erst doc, muss dazwischen ein ','
-   // getSingleDocRef ermoeglicht den Zugriff auf die werte in diesen "ordnern"
+
+  setNoteObject(obj: any): Note {
+    return {
+      type: obj.type || "note",
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false
+  }
+  }
+
+
+      // this.items$ = collectionData(this.getNotesRef());
+
+      // this.items$ = collectionData(this.getNotesRef(), { idField: 'id' })
+      // this.items = this.items$.subscribe((list) => {
+      //   list.forEach(element => {
+      //     // console.log(element);
+      //     console.log(this.setNoteObject(element, element.id))
+      //   })
+      // })
+
+      // bei collectionData erhalte ich ein Array aus Objekten der Dokumentendateien (aus der Firebase) - standardmaessig ohne id. 
+      // wenn ich die id haben moechte brauche ich { idField: 'id' } innerhalb der funktion:  this.items$ = collectionData(this.getNotesRef(), { idField: 'id' });
+
+      //bei collectionData wird nur bei aenderungen in der firebase die daten aktualisiert - und sonst nicht - d.h. es ist kein ngOnDestroy notwendig
+      // denn collectionData startet und beendet den Prozess automatisch - je nach Aenderungen in der Firebase.
+      // man kann aber ngOnDestroy werden, wenn man moechte, sobald man subscribe innherlab der collectionData funktion verwendet.
 
 }
